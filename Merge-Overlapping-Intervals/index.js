128e5336c058a4843464c8edf7977a23356a45e6
Tim Arioto <timarioto@gmail.com>
2017-05-03 11:21:25 -0700

    Merge-Overlapping-Intervals

---
+++ Merge-Overlapping-Intervals/index.js
@@ -0,0 +1,9 @@
+// Merge Overlapping Intervals
+// Description
+// Given an array (list) of intervals as input where each interval has a start and end timestamps.
+// Input array is sorted by starting timestamps.
+// You are required to merge overlapping intervals and return output array (list).
+
+// Consider below input array.
+// Intervals (1, 5), (3, 7), (4, 6), (6, 8) are overlapping so should be merged to one big interval (1, 8). 
+// Similarly intervals (10, 12) and (11, 15) are also overlapping intervals and should be merged to (10, 15).

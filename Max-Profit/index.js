486d6c1a6310eec6d888ba16591fcd1b1cb2c544
Tim Arioto <timarioto@gmail.com>
2017-05-11 13:02:34 -0700

    Add Max Profit

---
+++ Max-Profit/index.js
@@ -0,0 +1,7 @@
+// Find maximum single sell profit Given a list of stock prices for n days, find the maximum profit with a single buy/sell activity.
+// Description Given a list of day stock prices (integers for simplicity), find the maximum single sell profit.
+// We need to maximize the single buy/sell profit and in case we can't make any profit, we'll try to minimize the loss.
+// For below examples, buy and sell prices for making maximum profit are highlighted.
+//
+// Linear time, Constant space
+// Hint: Kadane’s algorithm.

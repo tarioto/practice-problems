688b32331c8de700b7304bf2b12beb1ac13db927
Tim Arioto <timarioto@gmail.com>
2017-05-17 17:36:32 -0700

    Add test cases

--- Compute-nth-Fibonacci-Number/index.js
+++ Compute-nth-Fibonacci-Number/index.js
@@ -1,1 +1,10 @@
 // Write a function fib() that a takes an integer n and returns the nth fibonacci number.
+
+
+
+
+fib(0); // => 0
+fib(1); // => 1
+fib(2); // => 1
+fib(3); // => 2
+fib(4); // => 3
